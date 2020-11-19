f077f6bd29a16e8aa4472d245db8c807baeabe7a
c-breiteneder <github@breiteneder.eu>
2020-11-19 18:49:38 +0100

    Added module configparser

--- attachments2nextcloud.py
+++ attachments2nextcloud.py
@@ -4,19 +4,18 @@ from imap_tools import MailBox,AND
 import owncloud
 import shutil
 import pytz
+import configparser
 
-###### Configuration Mailserver ##############
-imap_server = "<imap-server>"
-email_user = "<imap-user>"
-email_pass = "<imap-pass>"
-##############################################
 
-###### Configuration Nextcloud/Owncloud ######
-nc_server ="<cloud-server>"
-nc_user = "<cloud-user>"
-nc_pass  = "<cloud-pass>"
-nc_folder = "<path/to/destination>"
-##############################################
+config = configparser.ConfigParser()
+
+config.read('config.ini')
+
+
+config['Nextcloud']['server']
+config['Nextcloud']['username']
+config['Nextcloud']['password']
+config['Nextcloud']['destination']
 
 
 if os.path.isfile("lastrun.txt"):
@@ -36,8 +35,8 @@ timeString = lastRun.split(" ")[1]
 lastRunTime = datetime.time(int(timeString.split(":")[0]),int(timeString.split(":")[1]),int(timeString.split(":")[2]))
 
 lastRunDateTime = datetime.datetime.combine(lastRunDate,lastRunTime)
-nc = owncloud.Client(nc_server)
-nc.login(nc_user, nc_pass)
+nc = owncloud.Client(config['Nextcloud']['server'])
+nc.login(config['Nextcloud']['username'], config['Nextcloud']['password'])
 
 utc = pytz.utc
 
@@ -47,14 +46,14 @@ if os.path.isdir("tmp"):
 os.mkdir("tmp")
 
 i = 0
-with MailBox(imap_server).login(email_user, email_pass) as mailbox:
+with MailBox(config['IMAP']['server']).login(config['IMAP']['username'], config['IMAP']['password']) as mailbox:
     for msg in mailbox.fetch(AND(date_gte=lastRunDate)):
         if msg.date.replace(tzinfo=utc) > lastRunDateTime.replace(tzinfo=utc):
             for att in msg.attachments:
                 if att.filename != "":
                     with open("tmp\\"+format(att.filename), 'wb') as f:
                         f.write(att.payload)
-                        nc.put_file(nc_folder+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M_")+att.filename, "tmp//"+att.filename)
+                        nc.put_file(config['Nextcloud']['destination']+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M_")+att.filename, "tmp//"+att.filename)
                         i = i+1
 
 print ("{0} Files uploaded".format(i))
